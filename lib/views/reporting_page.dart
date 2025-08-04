eb5dd0cbc137235cdc1f5f9f484d4bbe808f5245
alperendonmez <alperendonmez@hotmail.com>
2025-08-04 14:57:36 +0300

    Added reporting page

---
+++ lib/views/reporting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReportingPage extends StatefulWidget {
+  const ReportingPage({super.key});
+
+  @override
+  State<ReportingPage> createState() => _ReportingPageState();
+}
+
+class _ReportingPageState extends State<ReportingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
