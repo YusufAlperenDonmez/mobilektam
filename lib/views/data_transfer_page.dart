bd3f1bbf8efdf3fe54869867ebbb5ebd26e9706f
alperendonmez <alperendonmez@hotmail.com>
2025-08-04 14:56:08 +0300

    Added data transfer page

---
+++ lib/views/data_transfer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataTransferPage extends StatefulWidget {
+  const DataTransferPage({super.key});
+
+  @override
+  State<DataTransferPage> createState() => _DataTransferPageState();
+}
+
+class _DataTransferPageState extends State<DataTransferPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
