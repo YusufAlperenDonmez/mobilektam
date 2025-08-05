ed3f0bc0257486c0b1eeea1449d76acd37a20351
alperendonmez <alperendonmez@hotmail.com>
2025-08-05 13:04:32 +0300

    updated note page ui

--- lib/views/note_page.dart
+++ lib/views/note_page.dart
@@ -10,6 +10,50 @@ class NotePage extends StatefulWidget {
 class _NotePageState extends State<NotePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final TextEditingController contextController = TextEditingController();
+    final TextEditingController noteController = TextEditingController();
+
+    final primaryColor = const Color.fromARGB(255, 67, 78, 177);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ürünler', style: TextStyle(color: Colors.white)),
+        backgroundColor: primaryColor,
+        iconTheme: const IconThemeData(color: Colors.white, size: 28),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            TextField(
+              controller: contextController,
+              decoration: const InputDecoration(
+                labelText: 'Konu',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: noteController,
+              maxLines: 10,
+              decoration: const InputDecoration(
+                labelText: 'Not',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle save action
+                final contextText = contextController.text;
+                final noteText = noteController.text;
+                // Save logic here
+                print('Context: $contextText, Note: $noteText');
+              },
+              child: const Text('Kaydet'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
